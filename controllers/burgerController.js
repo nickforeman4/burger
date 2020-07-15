2ce63880ccbc84572b1647f6d7f7487aefe89bb6
Nicholas Foreman <nicholas.m.foreman@gmail.com>
2020-07-14 21:17:18 -0500

    Updating orm.js

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -3,11 +3,11 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var cat = require("../models/burger.js");
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burger.all(function(data) {
     var hbsObject = {
       burgerData: data
     };
