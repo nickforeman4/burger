996f9bf8a37658d99d0abe9fda17440c764affeb
Nicholas Foreman <nicholas.m.foreman@gmail.com>
2020-07-15 22:31:05 -0500

    updated code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,9 +5,9 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-  burger.all(function(data) {
+  burger.selectAll(function(data) {
     var hbsObject = {
-      burgerData: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
@@ -15,7 +15,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.create([
+  burger.insertOne([
     "burger_name", "devoured"
   ], [
     req.body.name, false
@@ -29,7 +29,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.update({
+  burger.updateOne({
     devoured: true
   }, condition, function(result) {
     if (result.changedRows == 0) {
@@ -43,7 +43,7 @@ router.put("/api/burgers/:id", function(req, res) {
 router.delete("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  burger.delete(condition, function(result) {
+  burger.deleteOne(condition, function(result) {
     if (result.affectedRows == 0) {
       return res.status(404).end();
     } else {
--- models/burger.js
+++ models/burger.js
@@ -2,23 +2,23 @@ var orm = require("../config/orm.js");
 
 var burger = {
   selectAll: function(cb) {
-    orm.all("burgers", function(res) {
+    orm.selectAll("burgers", function(res) {
       cb(res);
     });
   },
   
   insertOne: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
+    orm.insertOne("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
   updateOne: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   },
   deletOne: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
+    orm.deleteOne("burgers", condition, function(res) {
       cb(res);
     });
   }
